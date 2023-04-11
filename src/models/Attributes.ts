115355a5c29789d33675e5059dab3a5e87ff6a42
Etienne Bellé <bl.etienne7@gmail.com>
2023-04-11 10:39:23 +0200

    171 Advanced Generic Constraints

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,13 +1,24 @@
+import { UserProps } from "./User";
+
 export class Attributes<T extends {}> {
 
-     constructor(private data: T) { }
+    constructor(private data: T) { }
     
-    get(propName: string): string | number {
-        return this.data[propName];
+    get<K extends keyof T>(key: K): T[K] { /* K : Key Of Object (exemple, par convention on utilise seulement la lettre K) */
+        return this.data[key];
     }
 
     set(dataUpdate: T): void {
         Object.assign(this.data, dataUpdate);
     }
-
 }
+
+const attrs = new Attributes<UserProps>({
+    id: 5,
+    age: 27,
+    name: 'Etienne'
+})
+
+const name = attrs.get('name')
+const age = attrs.get('age')
+const id = attrs.get('id')
